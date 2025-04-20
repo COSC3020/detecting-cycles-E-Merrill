3408c632e4e7fcc423c5d2c539a5791ad00d4897
E-Merrill <emerril3@uwyo.edu>
2025-04-20 14:20:51 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function hasCycle(graph) {
+    if (graph.length == 0){
+        return false;
+    }
+
+    for(let i = 0; i < graph.length; i++){
+        for (let j = 0; j < graph.length; j++){
+            if(graph[i][j]){
+                if(graph[j][i]){
+                    return true;
+                }
+            }
+        }
+    }
     return false;
 }
